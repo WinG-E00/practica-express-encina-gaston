aa6db1ef4af399e5f07153b0b2525b9e943ff73e
WinG-E00 <gastonencinawj@gmail.com>
2026-06-21 02:22:11 -0300

    feature/rutas-y-controladores:configuracion de los end points para el router

--- src/routes/personajes.routes.js
+++ src/routes/personajes.routes.js
@@ -1,5 +1,28 @@
 import { Router } from 'express';
-import {  } from '../controllers/personajes.controllers.js';
+
+import { crearUnPersonaje, obtenerTodosLosPersonajes,obtenerUnPersonajePorId, actualizarUnPersonaje, eliminarUnPersonaje } from '../controllers/personajes.controllers.js';
+
 
 
 export const personajesRouter = Router();
+
+//end point para obtener todos los personajes
+personajesRouter.get("/characters", obtenerTodosLosPersonajes);
+
+//end poiint para objetener solo un personaje por id
+personajesRouter.get("/characters/:id", obtenerUnPersonajePorId );
+
+
+//end point para post un personaje
+personajesRouter.post("/characters", crearUnPersonaje );
+
+// endpoint para put un personaje
+personajesRouter.put("/characters/:id", actualizarUnPersonaje);
+
+
+// end point para eliminar un personaje por id
+personajesRouter.delete("/characters/:id", eliminarUnPersonaje);
+
+
+
+
